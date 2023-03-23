26ac6eb94876449d3512b96300eaba446ed4eb95
ORead15 <128159996+ORead15@users.noreply.github.com>
2023-03-23 12:56:00 +0000

    Update Resize Raman contours to OM contours.py

--- Resize Raman contours to OM contours.py
+++ Resize Raman contours to OM contours.py
@@ -1,4 +1,4 @@
-#Script is used to scale the nanosheet from produced raman maps to the same aspect ratio as the corresponding optical image so
+#Script is used to scale the nanosheet shape from produced raman maps to the same aspect ratio as the corresponding optical image so
 #that the edges of the nanosheet can be extracted from the raman map, and compared to the edges from the optical image using the next script.
 
 #import modules
@@ -61,34 +61,35 @@ def get_OM_dimensions(image):
     return(roi_height, roi_width)
 
 #for each raman threshold map image in defined folder, get roi, use it to crop raman map and save it.
-for filename in glob.glob('D:\Anticounterfeiting Paper\POC Study\Flake Library\Raman Shapes\Raman Threshold/*.png'):
+for filename in glob.glob('Raman Shapes\Raman Threshold/*.png'):
     #trim sample/file name from filepath and remove .txt extension
     head, tail = os.path.split(filename)
     image_name = tail[:-4]
     
     (x, y, w, h) = get_raman_roi(filename) #get bounding box coords of raman map threshold image
     
-    raman_map = cv2.imread(f"D:\Anticounterfeiting Paper\POC Study\Flake Library\Raman Shapes\Raman area maps/{image_name}.png") #load raman area map
+    raman_map = cv2.imread(f"Raman Shapes\Raman area maps/{image_name}.png") #load raman area map
    
     raman_roi = raman_map[y:y + h, x:x + w]  #crop roi from map
     
-    cv2.imwrite(f"D:\Anticounterfeiting Paper\POC Study\Flake Library\Raman Shapes\Raman ROI/{image_name}.tif", raman_roi) #save raman roi 
+    cv2.imwrite(f"Raman Shapes\Raman ROI/{image_name}.tif", raman_roi) #save raman roi 
 
     
 #loop through all OM images in defined folder, crop roi, find dimensions of roi, load raman map and scale it to OM size, pad and save.
-for filename in glob.glob('D:\Anticounterfeiting Paper\POC Study\Flake Library\Raman Shapes\OM Raman/*.tif'):
+for filename in glob.glob('Raman Shapes\OM Raman/*.tif'):
     om_img = cv2.imread(filename) #read image
     roi, image_name = get_OM_shape(filename) #get roi and image name
-    cv2.imwrite(f"D:\Anticounterfeiting Paper\POC Study\Flake Library\Raman Shapes\OM resave/{image_name}.tif", om_img) #save the om_image to prevent resizing errors when loading/reloading images with cv2, cause unknown to me but it appears the image dpi changes but size etc stays the same.
+    cv2.imwrite(f"Raman Shapes\OM resave/{image_name}.tif", om_img) #save the om_image to prevent resizing errors when loading/reloading images with cv2, cause unknown to me but it appears the image dpi changes but size etc stays the same.
 
     height, width = get_OM_dimensions(roi) #get height and width of roi
     dimensions = (width, height) #reorder to use in cv2.resize
     
-    raman_roi = cv2.imread(f"D:\Anticounterfeiting Paper\POC Study\Flake Library\Raman Shapes\Raman ROI/{image_name}.tif") #load raman roi
+    raman_roi = cv2.imread(f"Raman Shapes\Raman ROI/{image_name}.tif") #load raman roi
      
     raman_rescale = cv2.resize(raman_roi, dimensions, interpolation= cv2.INTER_AREA) #rescale to aspect ratio of OM image
-    cv2.imwrite(f"D:\Anticounterfeiting Paper\POC Study\Flake Library\Raman Shapes\Rescaled raman/{image_name}.tif", raman_rescale) 
+    
+    cv2.imwrite(f"Raman Shapes\Rescaled raman/{image_name}.tif", raman_rescale) 
 
     raman_pad = cv2.copyMakeBorder(raman_rescale, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value = 0) #pad raman roi with 10 pixels so edges can be extracted
 
-    cv2.imwrite(f"D:\Anticounterfeiting Paper\POC Study\Flake Library\Raman Shapes\Rescaled Raman Pad/{image_name}.tif", raman_pad) #save rescale raman image
+    cv2.imwrite(f"Raman Shapes\Rescaled Raman Pad/{image_name}.tif", raman_pad) #save rescale raman image
